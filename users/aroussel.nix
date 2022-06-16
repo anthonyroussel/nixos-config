45fa0325a9002c9b5b19e4599054a71da5c2690d
Anthony Roussel <anthony.roussel17@gmail.com>
2022-06-16 10:45:00 +0200

    Add aroussel user to tss group if TPM2 security is enabled

--- users/aroussel.nix
+++ users/aroussel.nix
@@ -1,4 +1,4 @@
-{ inputs, config, sops, ... }:
+{ inputs, config, lib, sops, ... }:
 {
   sops.secrets."passwords/aroussel" = {
     neededForUsers = true;
@@ -12,7 +12,11 @@
         group = "aroussel";
         description = "Anthony Roussel";
         # Enable ‘sudo’ for the user.
-        extraGroups = [ "wheel" "docker" "networkmanager" ];
+        extraGroups = [ "wheel" ]
+          ++ lib.optionals config.networking.networkmanager.enable [ "networkmanager" ]
+          ++ lib.optionals config.virtualisation.docker.enable [ "docker" ]
+          ++ lib.optionals config.security.tpm2.enable [ "tss" ];
+
         # Generate the password with `mkpasswd -m sha-512 > passwords/aroussel`
         passwordFile = config.sops.secrets."passwords/aroussel".path;
         # Path to encrypted luks device that contains the user's home directory.
