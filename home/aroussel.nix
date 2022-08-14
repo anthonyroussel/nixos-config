8cc5b7aa7de1f386c8b41daa75e9c3a939f0ebc1
Anthony Roussel <anthony@roussel.dev>
2022-08-15 00:12:58 +0200

    Remove unused unstable packages

--- home/aroussel.nix
+++ home/aroussel.nix
@@ -38,6 +38,10 @@
     # System tools
     borgbackup # Borg backup tool
 
+    # Password management
+    inputs.nixpkgs-unstable.legacyPackages.x86_64-linux.gopass
+    inputs.nixpkgs-unstable.legacyPackages.x86_64-linux.gopass-jsonapi
+
     # Dev tools (use nix-direnv instead)
     #
     # Cloud tools
@@ -50,20 +54,6 @@
     inputs.nixpkgs-unstable.legacyPackages.x86_64-linux.chezmoi
     inputs.nixpkgs-unstable.legacyPackages.x86_64-linux.doctl
     inputs.nixpkgs-unstable.legacyPackages.x86_64-linux.gh
-    inputs.nixpkgs-unstable.legacyPackages.x86_64-linux.hugo
-
-    # Node.js
-    inputs.nixpkgs-unstable.legacyPackages.x86_64-linux.nodejs-18_x
-    inputs.nixpkgs-unstable.legacyPackages.x86_64-linux.nodePackages.yarn
-
-    # Password management
-    inputs.nixpkgs-unstable.legacyPackages.x86_64-linux.gopass
-    inputs.nixpkgs-unstable.legacyPackages.x86_64-linux.gopass-jsonapi
-
-    # Python
-    inputs.nixpkgs-unstable.legacyPackages.x86_64-linux.python3
-    inputs.nixpkgs-unstable.legacyPackages.x86_64-linux.python3Packages.pip
-    inputs.nixpkgs-unstable.legacyPackages.x86_64-linux.pipenv
 
     inputs.nixpkgs-unstable.legacyPackages.x86_64-linux.buku
 
