75e04974922b459f32018a798f7aa15b2d32b5f8
Anthony Roussel <anthony@roussel.dev>
2023-09-11 14:02:23 +0200

    fix(greetd): remove bootlog lines from tuigreet

--- modules/greetd/default.nix
+++ modules/greetd/default.nix
@@ -6,12 +6,29 @@
     settings = {
      default_session.command = ''
       ${pkgs.greetd.tuigreet}/bin/tuigreet \
-        --time \
         --asterisks \
+        --cmd sway \
+        --remember \
         --user-menu \
-        --cmd sway
+        --time \
+        --window-padding 2
     '';
     };
+  };
+
+  # this is a life saver.
+  # literally no documentation about this anywhere.
+  # might be good to write about this...
+  # https://www.reddit.com/r/NixOS/comments/u0cdpi/tuigreet_with_xmonad_how/
+  systemd.services.greetd.serviceConfig = {
+    Type = "idle";
+    StandardInput = "tty";
+    StandardOutput = "tty";
+    StandardError = "journal"; # Without this errors will spam on screen
+    # Without these bootlogs will spam on screen
+    TTYReset = true;
+    TTYVHangup = true;
+    TTYVTDisallocate = true;
   };
 
   environment.etc."greetd/environments".text = ''
