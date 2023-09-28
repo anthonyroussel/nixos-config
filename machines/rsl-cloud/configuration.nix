02807ee9ccf30603434301aa4b2fee66b0fadf89
Anthony Roussel <anthony@roussel.dev>
2023-09-28 07:42:15 +0200

    feat(rsl-cloud): sudo without password

--- machines/rsl-cloud/configuration.nix
+++ machines/rsl-cloud/configuration.nix
@@ -48,6 +48,8 @@
     };
   };
 
+  security.sudo.wheelNeedsPassword = false;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
