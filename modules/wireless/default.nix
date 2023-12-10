7af24d471d7d05228424c6d37f351ef204a3f210
Anthony Roussel <anthony@roussel.dev>
2023-12-10 21:26:20 +0100

    feat(wireless): switch to networkmanager.ensureProfiles
    https://github.com/NixOS/nixpkgs/pull/254647

--- modules/wireless/default.nix
+++ modules/wireless/default.nix
@@ -1,26 +1,64 @@
 { environment, config, sops, ... }:
-{
-  sops.secrets."wlan/d850" = { };
-  sops.secrets."wlan/6296" = { };
-  sops.secrets."wlan/e3f0" = { };
-  sops.secrets."wlan/redmi" = { };
 
-  environment.etc = {
-    "NetworkManager/system-connections/D850.nmconnection" = {
-      source = config.sops.secrets."wlan/d850".path;
-      mode = "0400";
+let
+  mkWirelessProfile = { uuid, ssid, psk }: {
+    connection = {
+      inherit uuid;
+      id = ssid;
+      type = "wifi";
+      interface-name = "wlan";
+      permissions = "";
     };
-    "NetworkManager/system-connections/6296.nmconnection" = {
-      source = config.sops.secrets."wlan/6296".path;
-      mode = "0400";
+    wifi = {
+      inherit ssid;
+      mac-address-blacklist = "";
+      mode = "infrastructure";
     };
-    "NetworkManager/system-connections/E3F0.nmconnection" = {
-      source = config.sops.secrets."wlan/e3f0".path;
-      mode = "0400";
+    wifi-security = {
+      inherit psk;
+      auth-alg = "open";
+      key-mgmt = "wpa-psk";
     };
-    "NetworkManager/system-connections/Redmi.nmconnection" = {
-      source = config.sops.secrets."wlan/redmi".path;
-      mode = "0400";
+    ipv4 = {
+      dns-search = "";
+      method = "auto";
+    };
+    ipv6 = {
+      addr-gen-mode = "stable-privacy";
+      dns-search = "";
+      method = "auto";
+    };
+  };
+
+in {
+  sops.secrets."networkmanager/Livebox-6296/psk" = { };
+  sops.secrets."networkmanager/Livebox-D850/psk" = { };
+  sops.secrets."networkmanager/Livebox-E3F0/psk" = { };
+
+  sops.templates."networkmanager".content = ''
+    LIVEBOX_6296_PSK = "${config.sops.placeholder."networkmanager/Livebox-6296/psk"}"
+    LIVEBOX_D850_PSK = "${config.sops.placeholder."networkmanager/Livebox-D850/psk"}"
+    LIVEBOX_E3F0_PSK = "${config.sops.placeholder."networkmanager/Livebox-E3F0/psk"}"
+  '';
+
+  networking.networkmanager.ensureProfiles = {
+    environmentFiles = [ /run/secrets-rendered/networkmanager ];
+    profiles = {
+      "Livebox-6296" = mkWirelessProfile {
+        uuid = "d68423d2-d65a-4a79-9f45-608d14b92d5a";
+        ssid = "Livebox-6296";
+        psk = "$LIVEBOX_6296_PSK";
+      };
+      "Livebox-D580" = mkWirelessProfile {
+        uuid = "b3039850-54f0-4859-ae26-728c4ee6f4c1";
+        ssid = "Livebox-D580";
+        psk = "$LIVEBOX_D580_PSK";
+      };
+      "Livebox-E3F0" = mkWirelessProfile {
+        uuid = "ade27a61-b441-49a2-bb43-5383a1c94a84";
+        ssid = "Livebox-E3F0";
+        psk = "$LIVEBOX_E3F0_PSK";
+      };
     };
   };
 }
