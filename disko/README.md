102d8068b2bfd953cb16ccd0aea847836a1a989e
Anthony Roussel <anthony@roussel.dev>
2023-12-11 14:40:17 +0100

    feat(disko): init

---
+++ disko/README.md
@@ -0,0 +1,1 @@
+See https://github.com/nix-community/disko/blob/master/docs/quickstart.md
---
+++ disko/rsl-xps.nix
@@ -0,0 +1,71 @@
+{
+  disko.devices = {
+    disk = {
+      vda = {
+        type = "disk";
+        device = "/dev/vda";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              type = "EF00";
+              size = "500M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            luks = {
+              size = "100%";
+              content = {
+                type = "luks";
+                name = "pool";
+                content = {
+                  type = "lvm_pv";
+                  vg = "pool";
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+
+    lvm_vg = {
+      pool = {
+        type = "lvm_vg";
+        lvs = {
+          home = {
+            size = "512M";
+            content = {
+              type = "btrfs";
+              mountpoint = "/home";
+              extraArgs = [ "-L" "home" ];
+              subvolumes = {
+                "/aroussel" = {
+                  mountpoint = "/home/aroussel";
+                };
+              };
+            };
+          };
+          nixos = {
+            size = "100%FREE";
+            content = {
+              type = "btrfs";
+              mountpoint = "/";
+              mountOptions = [ "defaults" ];
+              extraArgs = [ "-L" "nixos" ];
+              subvolumes = {
+                "/nix" = {
+                  mountOptions = [ "noatime" ];
+                  mountpoint = "/nix";
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
