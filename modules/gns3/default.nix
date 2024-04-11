ed1f5e3a6cf1c5fbe8609187f9badee6508eb2d6
Anthony Roussel <anthony@roussel.dev>
2024-04-11 19:55:51 +0200

    feat(gns3): run gns3-server directly from host

--- modules/gns3/default.nix
+++ modules/gns3/default.nix
@@ -11,58 +11,17 @@
 
   programs.gns3-gui.enable = true;
 
-  containers.gns3-server = {
-    autoStart = true;
-
-    bindMounts = {
-      "/run/secrets/gns3-password" = {
-        hostPath = config.sops.secrets."gns3/password".path;
-        isReadOnly = true;
-      };
+  services.gns3-server = {
+    enable = true;
+    dynamips.enable = true;
+    ubridge.enable = true;
+    vpcs.enable = true;
+    auth = {
+      enable = true;
+      user = "gns3";
+      passwordFile = config.sops.secrets."gns3/password".path;
     };
-
-    forwardPorts = [
-      {
-        protocol = "tcp";
-        hostPort = 3080;
-        containerPort = 3080;
-      }
-    ];
-
-    specialArgs = {
-      inherit (inputs) nur;
-    };
-
-    config =
-      { config, pkgs, ... }:
-      {
-        imports = [ nur.nixosModules.gns3-server ];
-
-        services.gns3-server = {
-          enable = true;
-          dynamips.enable = true;
-          ubridge.enable = true;
-          vpcs.enable = true;
-          auth = {
-            enable = true;
-            user = "gns3";
-            passwordFile = "/run/secrets/gns3-password";
-          };
-        };
-
-        system.stateVersion = "23.11";
-
-        networking = {
-          firewall = {
-            enable = true;
-            allowedTCPPorts = [ 3080 ];
-          };
-
-          # Use systemd-resolved inside the container
-          useHostResolvConf = lib.mkForce false;
-        };
-
-        services.resolved.enable = true;
-      };
   };
+
+
 }
